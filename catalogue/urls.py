2386494a03e308d062acd2af1a3868ab5ce15144
Elizaveta Nasytko <Liz99246@gmail.com>
2024-12-03 13:33:09 +0300

    django reworked

--- catalogue/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('mainpage/', views.home_page, name='home'),
-    path('contact_page/', views.contacts_page, name='contacts')
-]
--- catalogue/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def mainpage(request):
-    return render(request, 'catalogue/home.html')
-
-
-def contact_page(request):
-    return render(request, 'catalogue/contacts.html')
